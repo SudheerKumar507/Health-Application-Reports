0899c55b6f7c751dc179f77341dcbeb413e77920
Sudheer Kumar Anyam <sudheer.anyam@arthdesignbuild.com>
2017-11-01 19:28:57 +0530

    scaffold for Health Application

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name healthApplicationApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the healthApplicationApp
+ */
+angular.module('healthApplicationApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name healthApplicationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the healthApplicationApp
+ */
+angular.module('healthApplicationApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

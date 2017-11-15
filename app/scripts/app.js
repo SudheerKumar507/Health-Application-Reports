3b4884e6c3ac130ad6303f608ad3caf5f17d7505
Revathi Thotakura <revathidevithotakura@gmail.com>
2017-11-15 15:01:39 +0530

    session reports & viewer reports changed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,11 +30,13 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
-      }).when('/session-report', {
+      })
+      .when('/session-report', {
         templateUrl: 'views/session-report.html',
         controller: 'Session-ReportCtrl',
         controllerAs: 'session-report'
-      }).when('/viewer-report', {
+      })
+      .when('/viewer-report', {
         templateUrl: 'views/viewer-report.html',
         controller: 'Viewer-ReportCtrl',
         controllerAs: 'viewer-report'

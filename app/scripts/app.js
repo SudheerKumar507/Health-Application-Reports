d8e37f081a79276a03928e903509fb815076ac83
Rajsrii <rajasreeadabala21@gmail.com>
2017-11-15 15:51:48 +0530

    Reports Dashboard UI added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,6 +31,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/reports', {
+        templateUrl: 'views/reports.html',
+        controller: 'ReportsCtrl',
+        controllerAs: 'reports'
+      })
       .otherwise({
         redirectTo: '/'
       });
